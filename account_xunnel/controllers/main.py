bb12b2db901a5e38d4051989f0ad771d171a5651
Javier Vega <javier@vauxoo.com>
2018-07-16 09:58:19 -0500

    [FIX] account_xunnel: docstring to controller

--- account_xunnel/controllers/main.py
+++ account_xunnel/controllers/main.py
@@ -11,6 +11,10 @@ class MainController(Controller):
         '/account_xunnel/xunnel_webhook_connection/',
         type='json', auth='public', csrf=False)
     def webhook_hanlder(self, **kw):
+        """Recives a request from https://xunnel.com with new data for
+        auto-synchronize. It can either synchronize new transactions from
+        an existing account or add new accounts to your providers.
+        """
         post = request.jsonrequest
         provider = post.get('provider')
         event = post.get('handle')
