f89395e8991e414e2f39a1f148e296fba61e2566
Coward - Auto release <coward@vauxoo.com>
2018-08-09 01:28:56 +0000

    Bump version: 11.0.1.0.10 → 11.0.1.0.11

--- invoice_xunnel/__manifest__.py
+++ invoice_xunnel/__manifest__.py
@@ -6,7 +6,7 @@
     'summary': '''
         Use Xunnel Sync to retrieve bank statements
     ''',
-    'version': '11.0.1.0.10',
+    'version': '11.0.1.0.11',
     'author': 'Jarsa Sistemas,Vauxoo',
     'category': 'Accounting',
     'website': 'http://www.jarsa.com.mx',
