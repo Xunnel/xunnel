432501edb548acae8b6b42092f4783832140007a
Coward - Auto release <coward@vauxoo.com>
2018-08-21 17:58:19 +0000

    Bump version: 11.0.1.0.26 → 11.0.1.0.27

--- invoice_xunnel/__manifest__.py
+++ invoice_xunnel/__manifest__.py
@@ -6,7 +6,7 @@
     'summary': '''
         Use Xunnel Invoice to retrieve invoices from SAT.
     ''',
-    'version': '11.0.1.0.26',
+    'version': '11.0.1.0.27',
     'author': 'Jarsa Sistemas,Vauxoo',
     'category': 'Accounting',
     'website': 'http://www.jarsa.com.mx',
