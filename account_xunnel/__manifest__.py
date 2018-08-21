795aaa725e85ff1d966c0499948b5e8094f9d1f7
Coward - Auto release <coward@vauxoo.com>
2018-08-21 20:50:07 +0000

    Bump version: 11.0.1.0.27 → 11.0.1.0.28

--- account_xunnel/__manifest__.py
+++ account_xunnel/__manifest__.py
@@ -6,7 +6,7 @@
     'summary': '''
         Use Xunnel Bank to retrieve bank statements.
     ''',
-    'version': '11.0.1.0.27',
+    'version': '11.0.1.0.28',
     'author': 'Jarsa Sistemas,Vauxoo',
     'category': 'Accounting',
     'website': 'http://www.jarsa.com.mx',
--- invoice_xunnel/__manifest__.py
+++ invoice_xunnel/__manifest__.py
@@ -6,7 +6,7 @@
     'summary': '''
         Use Xunnel Invoice to retrieve invoices from SAT.
     ''',
-    'version': '11.0.1.0.27',
+    'version': '11.0.1.0.28',
     'author': 'Jarsa Sistemas,Vauxoo',
     'category': 'Accounting',
     'website': 'http://www.jarsa.com.mx',
