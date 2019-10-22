00731790ee72f57c55e7c21f03b944d98f1fdae4
Carmen Miranda <carmen@vauxoo.com>
2019-10-22 01:43:20 +0000

    [FIX] invoice_xunnel: Documents Thumbnails
    - Change thumbnail date, from the sync date to the stamped date.
    - Add Name's document and Total Amount inside the thumbnail under the title.
    - Emitter name field and total computed field are being stored.
    - Add help for the fields in the model "ir.attachment"
    - Add translations for the help and name of the fields.

--- invoice_xunnel/models/ir_attachment.py
+++ invoice_xunnel/models/ir_attachment.py
@@ -1,8 +1,9 @@
-
-from odoo import models, fields, api
-from lxml import objectify
+from datetime import datetime
 import base64
 import logging
+from lxml import objectify
+
+from odoo import models, fields, api
 
 _logger = logging.getLogger(__name__)
 
@@ -13,28 +14,44 @@ class IrAttachment(models.Model):
     emitter_partner_id = fields.Many2one(
         'res.partner',
         compute="_compute_emitter_partner_id",
-        required=True,
-        string="emitter")
-    xunnel_attachment = fields.Boolean()
+        string="Emitter",
+        help="In case this is a CFDI file, stores emitter's name.",
+        store=True)
+    xunnel_attachment = fields.Boolean(
+        help="Specify if this is a XUNNEL attachment.")
     invoice_total_amount = fields.Float(
-        compute="_compute_emitter_partner_id")
+        compute="_compute_emitter_partner_id",
+        help="In case this is a CFDI file, stores invoice's total amount.",
+        store=True)
+    stamp_date = fields.Datetime(
+        compute="_compute_emitter_partner_id",
+        help="In case this is a CFDI file, stores invoice's stamp date.")
 
     @api.multi
     @api.depends('datas')
     def _compute_emitter_partner_id(self):
-        for rec in self:
-            if not rec.xunnel_attachment:
-                continue
+        attachments = self.filtered(
+            lambda rec: rec.xunnel_attachment and rec.datas_fname and
+            rec.datas_fname.lower().endswith('xml'))
+        for rec in attachments:
             xml = rec.get_xml_object(rec.datas)
-            if not xml:
+            if xml is None:
                 return
             rfc = xml.Emisor.get('Rfc', '').upper()
             partner = self.env['res.partner'].search([
                 ('vat', '=', rfc), '|',
                 ('supplier', '=', True), ('customer', '=', True)
             ], limit=1)
+            stamp_date = xml.Complemento.xpath(
+                'tfd:TimbreFiscalDigital[1]',
+                namespaces={
+                    'tfd':
+                    'http://www.sat.gob.mx/TimbreFiscalDigital'})[0].get(
+                        'FechaTimbrado')
+
             rec.emitter_partner_id = partner.id
             rec.invoice_total_amount = xml.get('Total')
+            rec.stamp_date = datetime.strptime(stamp_date, "%Y-%m-%dT%H:%M:%S")
 
     @api.multi
     def get_xml_object(self, xml):
