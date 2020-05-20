ffbcb8675db740c180946ccf7d87eb1cda7b88e2
Coward - Auto release <coward@vauxoo.com>
2020-05-20 16:08:18 +0000

    Bump version: 12.0.1.0.50 → 12.0.1.0.51

--- account_xunnel/__manifest__.py
+++ account_xunnel/__manifest__.py
@@ -6,7 +6,7 @@
     'summary': '''
         Use Xunnel Bank to retrieve bank statements.
     ''',
-    'version': '12.0.1.0.50',
+    'version': '12.0.1.0.51',
     'author': 'Jarsa Sistemas,Vauxoo',
     'category': 'Accounting',
     'website': 'http://www.jarsa.com.mx',
