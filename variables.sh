b799e4b88b1245183dcb9e19b8f7f34bcf44b15c
Luis González <lgonzalez@vauxoo.com>
2020-11-26 20:01:53 +0000

    [FIX] .gitlab-ci: Force python3 when installing Deployv's gitlab-tools
    Python 2.7 support has been officially dropped, and so did DeployV.

--- variables.sh
+++ variables.sh
@@ -1,6 +1,6 @@
 export PSQL_VERSION="10"
 export VERSION="12.0"
-export TRAVIS_PYTHON_VERSION="3.5"
+export TRAVIS_PYTHON_VERSION="3.6"
 export ODOO_REPO="vauxoo/odoo"
 export ODOO_BRANCH="12.0"
 export BASE_IMAGE="vauxoo/odoo-120-image"
